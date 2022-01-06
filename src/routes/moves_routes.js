22576911b2a90f2b08efa7104796320e40b3e516
Martín Calderón <martincalderon92@outlook.com>
2022-01-06 18:15:53 -0300

    Add Post route for moves.

--- src/routes/moves_routes.js
+++ src/routes/moves_routes.js
@@ -15,4 +15,32 @@ router.get('/', (req, res) => {
 	});
 });
 
+router.post('/', (req, res) => {
+	let sqlPostMove = `
+        INSERT INTO Moves (move_description, move_amount, move_type, move_date)
+        VALUES (?, ?, ?, ?)
+    `;
+
+	let valuesPostMove = [
+		req.body.description,
+		req.body.amount,
+		req.body.type,
+		req.body.date,
+	];
+
+	connection.query(sqlPostMove, valuesPostMove, (err, result) => {
+		if (err) {
+			res.json({
+				status: 'Error.',
+				message: 'Error when trying to create a new move. Please try again.',
+			});
+		} else {
+			res.json({
+				status: 'Success',
+				message: 'Move created successfully.',
+			});
+		}
+	});
+});
+
 module.exports = router;
