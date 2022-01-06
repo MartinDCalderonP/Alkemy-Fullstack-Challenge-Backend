edf0fb708cd0fd369923c66badb2fd696c914c6b
Martín Calderón <martincalderon92@outlook.com>
2022-01-06 19:38:34 -0300

    Add CORS and Body Parser

--- src/server.js
+++ src/server.js
@@ -1,10 +1,24 @@
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
+
 const movesRoutes = require('./routes/moves_routes');
+
+const app = express();
 
 dotenv.config();
 
-const app = express();
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(
+	cors({
+		credentials: true,
+		origin: 'http://localhost:3000',
+		allowedHeaders: ['Access-Control-Allow-Origin', 'Content-Type'],
+	})
+);
 
 app.use('/moves', movesRoutes);
 
