acaf18e7305b1f0d6340e6e00e0726f837842ec8
Martín Calderón <martincalderon92@outlook.com>
2022-01-16 21:39:59 -0300

    Add auto sign in after sign up

--- src/routes/signUp_routes.js
+++ src/routes/signUp_routes.js
@@ -3,16 +3,16 @@ const router = express.Router();
 const connection = require('../connection');
 
 router.post('/users', (req, res) => {
-	let sqlSelectUser = `
+	const sqlCheckUser = `
 		SELECT *
 		FROM users
 		WHERE user_email = ?
 		AND user_password = ?
 	`;
 
-	let valuesSelectUser = [req.body.email, req.body.password];
+	const valuesCheckUser = [req.body.email, req.body.password];
 
-	connection.query(sqlSelectUser, valuesSelectUser, (err, result, fields) => {
+	connection.query(sqlCheckUser, valuesCheckUser, (err, result, fields) => {
 		if (err) {
 			res.json({
 				status: 'Error',
@@ -26,7 +26,7 @@ router.post('/users', (req, res) => {
 					message: 'User already exists.',
 				});
 			} else {
-				let sqlInsertUser = `
+				const sqlSignUpUser = `
 					INSERT INTO Users (
 						user_email,
 						user_password
@@ -36,11 +36,11 @@ router.post('/users', (req, res) => {
 					)
 				`;
 
-				let valuesInsertUser = [req.body.email, req.body.password];
+				const valuesSingUpUser = [req.body.email, req.body.password];
 
 				connection.query(
-					sqlInsertUser,
-					valuesInsertUser,
+					sqlSignUpUser,
+					valuesSingUpUser,
 					(err, result, fields) => {
 						if (err) {
 							res.json({
@@ -53,6 +53,47 @@ router.post('/users', (req, res) => {
 								status: 'Success',
 								message: 'User signed up successfully.',
 							});
+
+							const sqlSignInUser = `
+								SELECT *
+								FROM users
+								WHERE user_email = ?
+								AND user_password = ?
+							`;
+
+							const valuesSignInUser = [
+								req.body.email,
+								req.body.password,
+							];
+
+							connection.query(
+								sqlSignInUser,
+								valuesSignInUser,
+								(err, result, fields) => {
+									if (err) {
+										res.json({
+											status: 'Error',
+											message:
+												'Error when trying to sign in. Please try again.',
+										});
+									} else {
+										if (result.length > 0) {
+											req.session.user = result[0];
+
+											res.json({
+												status: 'Success',
+												message: 'Sign in successful.',
+												user: result[0],
+											});
+										} else {
+											res.json({
+												status: 'Error',
+												message: 'Invalid email or password.',
+											});
+										}
+									}
+								}
+							);
 						}
 					}
 				);
