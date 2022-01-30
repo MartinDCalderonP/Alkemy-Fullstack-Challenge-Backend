44b2ac4397d8c7893f2fed01ea85fea5a3ab4e1e
Martín Calderón <martincalderon92@outlook.com>
2022-01-30 01:58:48 -0300

    Add JWT when sign in after sign up

--- src/routes/signUp_routes.ts
+++ src/routes/signUp_routes.ts
@@ -1,5 +1,9 @@
 import express from 'express';
 import connection from '../connection';
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+
+dotenv.config();
 
 const router = express.Router();
 
@@ -13,90 +17,88 @@ router.post('/users', (req, res) => {
 
 	const valuesCheckUser = [req.body.email, req.body.password];
 
-	connection.query(sqlCheckUser, valuesCheckUser, (err, result, fields) => {
+	connection.query(sqlCheckUser, valuesCheckUser, (err, result) => {
 		if (err) {
-			res.json({
+			return res.json({
 				status: 'Error',
 				message:
 					'Error when trying to check if user already exists. Please try again.',
 			});
-		} else {
-			if (result.length > 0) {
-				res.json({
+		}
+
+		if (result.length > 0) {
+			return res.json({
+				status: 'Error',
+				message: 'User already exists.',
+			});
+		}
+
+		const sqlSignUpUser = `
+			INSERT INTO Users (
+				user_name,
+				user_email,
+				user_password
+			) VALUES (
+				?,
+				?,
+				?
+			)
+		`;
+
+		const valuesSingUpUser = [req.body.name, req.body.email, req.body.password];
+
+		connection.query(sqlSignUpUser, valuesSingUpUser, (err) => {
+			if (err) {
+				return res.json({
 					status: 'Error',
-					message: 'User already exists.',
+					message: 'Error when trying to sign up a new user. Please try again.',
 				});
-			} else {
-				const sqlSignUpUser = `
-					INSERT INTO Users (
-						user_name,
-						user_email,
-						user_password
-					) VALUES (
-						?,
-						?,
-						?
-					)
+			}
+
+			const sqlSignInUser = `
+					SELECT *
+					FROM users
+					WHERE user_email = ?
+					AND user_password = ?
 				`;
 
-				const valuesSingUpUser = [
-					req.body.name,
-					req.body.email,
-					req.body.password,
-				];
+			const valuesSignInUser = [req.body.email, req.body.password];
 
-				connection.query(
-					sqlSignUpUser,
-					valuesSingUpUser,
-					(err, result, fields) => {
-						if (err) {
-							res.json({
-								status: 'Error',
-								message:
-									'Error when trying to sign up a new user. Please try again.',
-							});
-						} else {
-							const sqlSignInUser = `
-								SELECT *
-								FROM users
-								WHERE user_email = ?
-								AND user_password = ?
-							`;
+			connection.query(sqlSignInUser, valuesSignInUser, (err, result) => {
+				if (err) {
+					return res.json({
+						status: 'Error',
+						message:
+							'Error when trying to sign in after sign up a new user. Please try again.',
+					});
+				}
 
-							const valuesSignInUser = [req.body.email, req.body.password];
+				if (result.length > 0 && process.env.JWT_SECRET) {
+					const userForToken = {
+						id: result[0].user_id,
+						name: result[0].user_name,
+						email: result[0].user_email,
+					};
 
-							connection.query(
-								sqlSignInUser,
-								valuesSignInUser,
-								(err, result, fields) => {
-									if (err) {
-										res.json({
-											status: 'Error',
-											message:
-												'Error when trying to sign in after sign up a new user. Please try again.',
-										});
-									} else {
-										if (result.length > 0) {
-											res.json({
-												status: 'Success',
-												message: 'User signed up successfully.',
-												user: result[0],
-											});
-										} else {
-											res.json({
-												status: 'Error',
-												message:
-													'Error when trying to sign in after sign up a new user. Please try again.',
-											});
-										}
-									}
-								}
-							);
-						}
-					}
-				);
-			}
-		}
+					const token = jwt.sign(userForToken, process.env.JWT_SECRET, {
+						expiresIn: '1h',
+					});
+
+					return res.json({
+						status: 'Success',
+						message: 'User signed up successfully.',
+						user: result[0],
+						token: token,
+					});
+				}
+
+				return res.json({
+					status: 'Error',
+					message:
+						'Error when trying to sign in after sign up a new user. Please try again.',
+				});
+			});
+		});
 	});
 });
 
